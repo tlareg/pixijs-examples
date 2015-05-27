6cf40b877139d0061df8c1662d019c789402aa7b
Tlareg Aivir <tlaregaivir@gmail.com>
2015-05-27 22:02:48 +0200

    blah blah

--- src/app/js/main.js
+++ src/app/js/main.js
@@ -1,24 +1,25 @@
-var renderer = PIXI.autoDetectRenderer(800, 600,{backgroundColor : 0x1099bb});
-document.body.appendChild(renderer.view);
+
+var gameCanvas = document.getElementById('game-canvas');
+var renderer = PIXI.autoDetectRenderer(800, 600, { 
+  backgroundColor: 0x1099bb,
+  view: gameCanvas 
+});
 
 // create the root of the scene graph
 var stage = new PIXI.Container();
 
-// create a texture from an image path
-var texture = PIXI.Texture.fromImage('assets/bunny.png');
+var hero = createSprite();
 
-// create a new Sprite using the texture
-var bunny = new PIXI.Sprite(texture);
+stage.addChild(hero);
+stage.addChild(createText('Wombat life'));
 
-// center the sprite's anchor point
-bunny.anchor.x = 0.5;
-bunny.anchor.y = 0.5;
-
-// move the sprite to the center of the screen
-bunny.position.x = 200;
-bunny.position.y = 150;
-
-stage.addChild(bunny);
+var graphics = new PIXI.Graphics();
+// draw a circle, set the lineStyle to zero so the circle doesn't have an outline
+graphics.lineStyle(0);
+graphics.beginFill(0xFFFF0B, 0.5);
+graphics.drawCircle(250, 200,20);
+graphics.endFill();
+stage.addChild(graphics);
 
 // start animating
 animate();
@@ -26,8 +27,57 @@ function animate() {
     requestAnimationFrame(animate);
 
     // just for fun, let's rotate mr rabbit a little
-    bunny.rotation += 0.5;
+    hero.rotation += 0.1;
 
     // render the container
     renderer.render(stage);
+}
+
+
+function createSprite() {
+  // create a texture from an image path
+  var texture = PIXI.Texture.fromImage('assets/bunny.png');
+
+  // create a new Sprite using the texture
+  var bunny = new PIXI.Sprite(texture);
+
+  // center the sprite's anchor point
+  bunny.anchor.x = 0.5;
+  bunny.anchor.y = 0.5;
+
+  // move the sprite to the center of the screen
+  bunny.position.x = 200;
+  bunny.position.y = 150;
+
+  bunny.interactive = true;
+  bunny.on('mousedown', onDown);
+  bunny.on('touchstart', onDown);
+
+  function onDown(eventData) {
+    bunny.scale.x += 0.3;
+    bunny.scale.y += 0.3;
+  }
+
+  return bunny;
+}
+
+function createText(txt) {
+  var style = {
+    font : '36px Arial bold italic',
+    fill : '#F7EDCA',
+    stroke : '#4a1850',
+    strokeThickness : 5,
+    dropShadow : true,
+    dropShadowColor : '#000000',
+    dropShadowAngle : Math.PI / 6,
+    dropShadowDistance : 6,
+    wordWrap : true,
+    wordWrapWidth : 440
+  };
+
+  var richText = new PIXI.Text(txt, style);
+  richText.x = 30;
+  richText.y = 180;
+
+  return richText;
 }
